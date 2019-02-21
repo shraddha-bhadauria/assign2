c8cbecbad4d2703e50e13a92bdca34045977b865
Harsha1908 <45085330+Harsha1908@users.noreply.github.com>
2019-02-20 22:04:16 -0500

    Update tour.rb

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,32 +1,20 @@
 class Tour < ApplicationRecord
-  belongs_to :agent
+  has_and_belongs_to_many :customers
 
+  validates :id, presence: true, uniqueness: {case_sensitive: false}
   validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :description, presence: true
   validates :photos, presence: true
+  #mount_uploader :image, ImageUploader
+  serialize :image, JSON
   validates :price, presence: true, numericality: true
-  validates :booking_date_validity, on: :create, on: :update
-  validates :valid_start_date, on: :create, on: :update
-  validates :valid_end_date, on: :create, on: :update
+  #validates :booking_date_validity, presence: true, on: :create, on: :update
+  validates :booking_date, presence: true
+  #validates :valid_start_date, presence: true, on: :create, on: :update
+  validates :start_date, presence: true
+  #validates :valid_end_date, presence: true, on: :create, on: :update
+  validates :end_date, presence: true
   validates :start_location, presence: true
-  validates :contact_agent, presence: true
+  validates :operator_contact, presence: true
   validates :status, presence:true, inclusion: { in: %w(InFuture Completed Cancelled)}
-
-  def booking_date_validity
-    if (:booking_deadline.present? && :booking_deadline < Date.today)
-      errors.add(:expiration_date, "can't be in the past")
-    end
   end
-
-  def valid_start_date
-    if (:from_date.present? && :from_date < Date.today)
-      errors.add(:start_date, "can't be in the past")
-    end
-  end
-
-  def valid_end_date
-    if :to_date.present? && :to_date < Date.today
-      errors.add(:end_date, "can't be in the past")
-    end
-  end
-end
