66faf0c85542731a1923c97c8b0b24abb766c7f3
Harsha1908 <45085330+Harsha1908@users.noreply.github.com>
2019-02-20 22:08:52 -0500

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,33 @@
 class ReviewsController < ApplicationController
-  def index
+  def review_params
+    params.require(:review).permit(:id, :customer_id, :subject, :message_content)
   end
+   def index
+
+     @reviews = Review.all
+   end
 
   def new
+    @reviews = Review.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @reviews }
+    end
   end
 
   def create
+    @reviews = Review.new(review_params)
+
+    respond_to do |format|
+      if @reviews.save
+        format.html { redirect_to @reviews, notice: 'a review of a customer has been created.' }
+        format.json { render json: @reviews, status: :created, location: @reviews }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @reviews.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
@@ -15,10 +37,28 @@ class ReviewsController < ApplicationController
   end
 
   def update
+    @reviews = Review.find(params[:id])
+
+    respond_to do |format|
+      if @reviews.update_attributes(review_params)
+        format.html { redirect_to @reviews, notice: 'Review successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @reviews.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
 
   def destroy
+    @reviews = Review.find(params[:id])
+    @reviews.destroy
+    flash[:notice] = "review has been deleted."
+    respond_to do |format|
+      format.html { redirect_to_reviews_url }
+      format.json { head :no_content }
+    end
   end
-
 
 end
