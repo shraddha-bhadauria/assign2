f5f3db95214e47080ea35414e5987c2d6eb50f26
Harsha1908 <45085330+Harsha1908@users.noreply.github.com>
2019-02-20 22:05:03 -0500

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :customer
-  belongs_to :tour
-
-  validates :subject, presence: true
-  validates :body, presence: true
-
-
+  has_and_belongs_to_many :customers
+  validates :id, presence: true, uniqueness: {case_sensitive: false}
+  validates :customer_id, presence: true
+  validates :subject, presence: true , length: {maximum: 50}
+  validates :body, presence: true , length: {maximum: 150}
 end
